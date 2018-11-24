8f91a7ada5a65e1a163e2c8966f6b97daafb369c
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-11-24 10:07:27 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,8 +1,13 @@
 n=int(raw_input())
 l=[int(x) for x in raw_input().split()]
+min=-1
 r=[]
 for i in range(0,n):
 	for j in range(i+1,n):
 		if l[i]==l[j]:
+			min=i
 			r.append(l[j])
-print r[0]
+if min!=-1:
+	print r[0]
+else:
+	print "unique"
