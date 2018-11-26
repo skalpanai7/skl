908fa06d6f0945d68979b854c6c7d50e7207137e
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-11-26 20:49:30 +0530

    Update sum0.py

--- sum0.py
+++ sum0.py
@@ -1,10 +1,15 @@
-n=int(raw_input())
+n=input()
 l=[int(x) for x in raw_input().split()]
-r=[]
-for i in range(0,n):
-	for j in range(i+1,n):
-		if l[i]+l[j]==0:
-			r.append(l[i])
-			r.append(l[j])
-print " ".join(map(str,r))
-			
+l.sort()
+i=0;j=n-1
+min=9999
+while i<j:
+	if min>abs(l[i]+l[j]):
+		min=abs(l[i]+l[j])
+		x=l[i]
+		y=l[j]
+	if l[i]+l[j]<0:
+		i+=1
+	else:
+		j-=1
+print x,y
