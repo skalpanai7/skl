2a3907b3610419010cc139e198d802f1301ae52c
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-11-26 20:31:30 +0530

    Update numstr.py

--- numstr.py
+++ numstr.py
@@ -1,7 +1,7 @@
-n=int(raw_input())
-l=[int(x) for x in raw_input().split()]
-for i in range(0,n):
-	for j in range(i+1,n):
-		for k in range(j+1,n):
-			if l[i]+l[j]==l[k]:
-				print l[i],l[j],l[k]
+num=int(raw_input())
+list=[int(x) for x in raw_input().split()]
+for i in range(0,num):
+	for j in range(i+1,num):
+		for k in range(j+1,num):
+			if list[i]+list[j]==list[k]:
+				print list[i],list[j],list[k]
