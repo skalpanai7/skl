51c9611203135597a0fa9d35b38d98c3c9e4c6bb
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-11-26 20:05:26 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,12 +1,12 @@
-n=int(raw_input())
-l=[int(x) for x in raw_input().split()]
+num=int(raw_input())
+list=[int(x) for x in raw_input().split()]
 min=-1
 r=[]
-for i in range(0,n):
-	for j in range(i+1,n):
-		if l[i]==l[j]:
+for i in range(0,num):
+	for j in range(i+1,num):
+		if list[i]==list[j]:
 			min=i
-			r.append(l[j])
+			r.append(list[j])
 if min!=-1:
 	print r[0]
 else:
